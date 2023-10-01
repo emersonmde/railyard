ed796cac137d3f72d050a3c55735339c4676f481
Matthew Emerson <emersonmde@protonmail.com>
2023-10-01 18:05:25 -0400

    Fixed proto imports

--- src/cluster_management_service.rs
+++ src/cluster_management_service.rs
@@ -2,19 +2,23 @@ use tonic::{Request, Response, Status};
 use crate::railyard::{AppendEntriesRequest, AppendEntriesResponse, InstallSnapshotRequest, InstallSnapshotResponse, RequestVoteRequest, RequestVoteResponse};
 use crate::railyard::cluster_management_server::ClusterManagement;
 
+#[derive(Debug, Default)]
+
 pub struct ClusterManagementService {}
 
 
+#[tonic::async_trait]
+
 impl ClusterManagement for ClusterManagementService {
-    async fn append_entries(&self, request: Request<AppendEntriesRequest>) -> Result<Response<AppendEntriesResponse>, Status> {
+    async fn append_entries(&self, _request: Request<AppendEntriesRequest>) -> Result<Response<AppendEntriesResponse>, Status> {
         todo!()
     }
 
-    async fn request_vote(&self, request: Request<RequestVoteRequest>) -> Result<Response<RequestVoteResponse>, Status> {
+    async fn request_vote(&self, _request: Request<RequestVoteRequest>) -> Result<Response<RequestVoteResponse>, Status> {
         todo!()
     }
 
-    async fn install_snapshot(&self, request: Request<InstallSnapshotRequest>) -> Result<Response<InstallSnapshotResponse>, Status> {
+    async fn install_snapshot(&self, _request: Request<InstallSnapshotRequest>) -> Result<Response<InstallSnapshotResponse>, Status> {
         todo!()
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,23 @@
-use railyard_service::RailyardService;
-use railyard::railyard_server::RailyardServer;
-use railyard::cluster_management_server::ClusterManagementServer;
 use tonic::transport::Server;
+
+use cluster_management_service::ClusterManagementService;
+use railyard::cluster_management_server::ClusterManagementServer;
+use railyard::railyard_server::RailyardServer;
+use railyard_service::RailyardService;
 
 mod railyard_service;
 mod cluster_management_service;
 
 pub mod railyard {
     tonic::include_proto!("railyard");
-    tonic::include_proto!("cluster_management");
 }
-
-struct
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:8000".parse()?;
     let management_addr = "[::1]:8001".parse()?;
     let railyard_service = RailyardService::default();
+    let management_service = ClusterManagementService::default();
 
     Server::builder()
         .add_service(RailyardServer::new(railyard_service))
@@ -25,7 +25,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     Server::builder()
-        .add_service(ClusterManagementServer::new(railyard_service))
+        .add_service(ClusterManagementServer::new(management_service))
         .serve(management_addr)
         .await?;
 
