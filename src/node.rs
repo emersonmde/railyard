45b281195d84a1a041937ebedc403248dd101427
Matthew Emerson <emersonmde@protonmail.com>
2023-09-30 21:11:02 -0400

    Updating API Methods for Railyard service

---
+++ src/node.rs
@@ -0,0 +1,41 @@
+use tonic::{Request, Response, Status};
+
+use crate::railyard::railyard_server::Railyard;
+use crate::railyard::{
+    CreateStreamRequest, CreateStreamResponse, GetRecordsRequest, GetRecordsResponse,
+    GetShardIndexRequest, GetShardIndexResponse, PutRecordRequest, PutRecordResponse,
+};
+
+#[derive(Debug, Default)]
+pub struct Node {}
+
+#[tonic::async_trait]
+impl Railyard for Node {
+    async fn create_stream(
+        &self,
+        _request: Request<CreateStreamRequest>,
+    ) -> Result<Response<CreateStreamResponse>, Status> {
+        todo!()
+    }
+
+    async fn get_records(
+        &self,
+        _request: Request<GetRecordsRequest>,
+    ) -> Result<Response<GetRecordsResponse>, Status> {
+        todo!()
+    }
+
+    async fn put_record(
+        &self,
+        _request: Request<PutRecordRequest>,
+    ) -> Result<Response<PutRecordResponse>, Status> {
+        todo!()
+    }
+
+    async fn get_shard_iterator(
+        &self,
+        _request: Request<GetShardIndexRequest>,
+    ) -> Result<Response<GetShardIndexResponse>, Status> {
+        todo!()
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -1,33 +1,17 @@
-use tonic::{Request, Response, Status};
+use node::Node;
+use railyard::railyard_server::RailyardServer;
 use tonic::transport::Server;
-use railyard::railyard_server::{RailyardServer, Railyard};
-use railyard::{HealthCheckRequest, HealthCheckResponse};
+
+mod node;
 
 pub mod railyard {
     tonic::include_proto!("railyard");
 }
 
-#[derive(Debug, Default)]
-pub struct RailyardNode {}
-
-#[tonic::async_trait]
-impl Railyard for RailyardNode {
-    async fn health_check(
-        &self,
-        request: Request<HealthCheckRequest>,
-    ) -> Result<Response<HealthCheckResponse>, Status> {
-        let reply = HealthCheckResponse {
-            status: true,
-            nonce: format!("Hello {}!", request.into_inner().nonce),
-        };
-        Ok(Response::new(reply))
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let node = RailyardNode::default();
+    let addr = "[::1]:8000".parse()?;
+    let node = Node::default();
 
     Server::builder()
         .add_service(RailyardServer::new(node))
